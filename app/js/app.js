1a77376b870d9ac54a4d07084049d6f6418f1e08
Edmond-999 <82581663+Edmond-999@users.noreply.github.com>
2022-03-09 17:05:33 +0400

    add burger click function

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,14 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".lines").click(function () {
+    if ($(".lines").hasClass("opened")) {
+      $(".lines").removeClass("opened");
+    } else {
+      $(".lines").addClass("opened");
+    }
+    if ($(".nav-ul").hasClass("mobile")) {
+      $(".nav-ul").removeClass("mobile");
+    } else {
+      $(".nav-ul").addClass("mobile");
+    }
+  });
+});
