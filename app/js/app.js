c3628c19f3bbd8352051f6d0d535794638576f30
Hov26 <hov.ghevondyan1994@gmail.com>
2022-03-11 16:10:51 +0400

    fix srtyles

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-    $(".submit").click(function () {
-      if ($(".accept").hasClass("submited")) {
-        $(".accept").removeClass("submited");
-      } else {
-        $(".accept").addClass("submited");
-      }
-    });
-  });
+  $(".submit").click(function() {
+    $(".modal-backdrop").addClass('show')
+    setTimeout(function() {
+      $(".modal-backdrop").removeClass('show')
+    }, 3000)
+  })
+})
