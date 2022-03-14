eb496cef5a823cf2dcae52204efd38b6b35360de
Hovhannes <hov.ghevondyan1994@gmail.com>
2022-03-14 16:14:25 +0400

    Add slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-  $(".submit").click(function() {
-    $(".modal-backdrop").addClass('show')
-    setTimeout(function() {
-      $(".modal-backdrop").removeClass('show')
-    }, 3000)
-  })
-})
+  $(".submit").click(function () {
+    $(".modal-backdrop").addClass("show");
+    setTimeout(function () {
+      $(".modal-backdrop").removeClass("show");
+    }, 3000);
+  });
+
+  $(".owl-carousel").owlCarousel({
+    dots: true,
+    nav: false,
+    navElement: "div",
+    items: 1,
+    margin: 0,
+    autoplay: false,
+  });
+});
