53ae26bf4a6694d063dc977c273d3b7264310816
Hov26 <hov.ghevondyan1994@gmail.com>
2022-03-21 15:18:57 +0400

    ui fixes

--- app/js/app.js
+++ app/js/app.js
@@ -31,9 +31,9 @@ $(document).ready(function () {
 
   $(window).scroll(function(){
     if ($(window).scrollTop() > 10) {
-      $('.header').addClass('shadow')
+      $('.header').addClass('shadow bg-blue')
     } else {
-      $('.header').removeClass('shadow')
+      $('.header').removeClass('shadow bg-blue')
     }
   })
 
